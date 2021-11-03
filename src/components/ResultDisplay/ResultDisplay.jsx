5d1daff9aa079312a784d34eda1b738abb7ae30c
brunão <btex88@gmail.com>
2021-11-02 22:22:59 -0300

    ResultDisplay refactoring - .map() usage

--- src/components/ResultDisplay/ResultDisplay.jsx
+++ src/components/ResultDisplay/ResultDisplay.jsx
@@ -1,50 +1,49 @@
 import React from 'react';
-import ResultSubtitle from '../ResultSubtitle/ResultSubtitle';
 import Output from '../Output/Output';
 import { generateTime } from '../../data/data';
 
 class ResultDisplay extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
+    this.timeObj = {
       entree: localStorage.getItem('entree'),
       workday: localStorage.getItem('workday'),
       lunchtime: localStorage.getItem('lunchtime'),
       pause: localStorage.getItem('pause'),
       delay: localStorage.getItem('delay'),
     }
+
+    this.labelObj = {
+      name: [
+        'Entrada:',
+        'Início da Pausa:',
+        'Fim da Pausa:',
+        'Saída:'
+      ],
+    }
   }
 
   render() {
-    const { entree, workday, lunchtime, pause, delay } = this.state
+    const { entree, workday, lunchtime, pause, delay } = this.timeObj
+    const { name } = this.labelObj
+
     const result = generateTime(
       Number(entree),
       Number(workday),
       Number(lunchtime),
       Number(pause),
       Number(delay),
-      );
-  
+    );
+
     return (
       <div>
-        <ResultSubtitle />
-
-        <Output
-          name='Entrada:'
-          time={result[0]}
-        />
-        <Output
-          name='Início da Pausa:'
-          time={result[1]}
-        />
-        <Output
-          name='Fim da Pausa:'
-          time={result[2]}
-        />
-        <Output
-          name='Saída:'
-          time={result[3]}
-        />
+        {name.map((value, index) => (
+          <Output
+            key={ Object.keys(this.timeObj)[index] }
+            name={ value }
+            time={ result[index] }
+          />
+        ))}
       </div>
     )
   }
